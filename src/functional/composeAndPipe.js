7b62abeed2e0e54ab8cf7df7c5751b09275ff4b7
qiuxan <qiuxan@gmail.com>
2021-10-12 06:11:29 +1100

    compose pipe to merge functions with one function

---
+++ src/functional/composeAndPipe.js
@@ -0,0 +1,22 @@
+
+import { compose, pipe } from 'lodash/fp'
+
+// trim
+// wrapInDiv
+let input = "  JavaScript  "
+//let output = "<div>"+ input.trim() +"</div>"
+
+const trim = str => str.trim()
+const wrapInDiv = str => `<div>${str}</div>`
+//const wrapInSpan = str => `<span>${str}</span>`
+//const wrap = (type, str) => `<${type}>${str}</${type}>`
+const toLowerCase = str => str.toLowerCase()
+
+//const result = wrapInDiv(toLowerCase(trim(input)))
+
+const transformCompose = compose(wrapInDiv, toLowerCase, trim)
+
+const transform = pipe(trim, toLowerCase, wrapInDiv)
+
+console.log(transformCompose(input));
+console.log(transform(input));
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
-    port: 9000
+    port: 9001
   },
   mode: "development"
 };
